f31de881729f14e6c0753d81e68272064b4d2312
AjmalAssouma <138770455+AjmalAssouma@users.noreply.github.com>
2024-01-09 21:29:23 +0000

    Update user.js

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -35,7 +35,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://eshop-online-front.vercel.app/activation/${activationToken}`;
+    const activationUrl = `https://eshop-front.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
