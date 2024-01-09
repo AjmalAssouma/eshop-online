1820856039ec198e601e6e8258f11430033aad63
AjmalAssouma <138770455+AjmalAssouma@users.noreply.github.com>
2024-01-09 20:51:47 +0100

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
-export const server = "http://localhost:8000/api/v2";
+// export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+// export const backend_url = "http://localhost:8000/";
 
-// export const server = "https:eshop-tutorial.vercel.app/api/v2";
+export const server = "https://eshop-online-back.vercel.app/api/v2";
