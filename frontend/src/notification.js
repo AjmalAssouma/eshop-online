114eb91c4498004e41e40568b15b2937387ec4ff
Ajmal <ajmalassouma2@gmail.com>
2024-01-09 20:43:23 +0100

    first commit

---
+++ frontend/src/notification.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect } from "react";
+import { Toaster, toast } from "react-hot-toast";
+import { requestPermission, onMessageListener } from "./firebase";
+
+function Notification() {
+    const [notification, setNotification] = useState({ title: "", body: "" });
+
+    useEffect(() =>{
+        requestPermission();
+
+        const unsubscribe = onMessageListener().then(payload => {
+            setNotification({
+                title: payload?.notification?.title,
+                body: payload?.notification?.body
+            });
+
+            toast.success(`${payload?.notification?.title}: ${payload?.notification?.body}`,
+            {
+                duration: 3000, // Display the notification for 1 minute
+                position: "bottom-right"  // Display the notification at the top right section
+            }
+            );
+        });
+
+        return () => {
+            unsubscribe.catch(err => console.log("failed: ", err));
+        };
+    }, []);
+
+    return(
+        <div>
+            <Toaster />
+        </div>
+    );
+}
+
+export default Notification;
