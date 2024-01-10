736ca0d5482535b61fca65a4e975bbd5be284ace
AjmalAssouma <138770455+AjmalAssouma@users.noreply.github.com>
2024-01-10 16:11:55 +0100

    Update swDev.js

--- frontend/src/swDev.js
+++ frontend/src/swDev.js
@@ -1,10 +1,31 @@
 export default function swDev(){
 
+  // const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+
+  // if ('serviceWorker' in navigator) {
+  //   window.addEventListener('load', () => {
+  //     navigator.serviceWorker
+  //     .register(swUrl)
+  //     .then((registration) => {
+  //       console.log('Service Worker Registered');
+  //       console.log('Registration', registration);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Service Worker registration failed:', error);
+  //     });
+  //   })
+  // } else {
+  //   console.warn('Service Worker is not supported in this browser');
+  // }
+
+
+
+
   const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
       .register(swUrl)
       .then((registration) => {
         console.log('Service Worker Registered');
@@ -13,9 +34,10 @@ export default function swDev(){
       .catch((error) => {
         console.error('Service Worker registration failed:', error);
       });
-    })
-  } else {
-    console.warn('Service Worker is not supported in this browser');
-  }
+  });
+} else {
+  console.warn('Service Worker is not supported in this browser or not in production mode');
+}
+
 
 }
